17ed7ecbc85362d5b262eba99b8b0e365076a9a2
Matthias Hryniszak <padcom@gmail.com>
2022-07-29 00:37:35 +0200

    Updated the demo application to read package import definitions from importmap directly

---
+++ docs/libraries.js
@@ -0,0 +1,66 @@
+const LIBRARIES = JSON.parse(document.scripts.imports.innerText).imports
+
+export function isLibraryRegistered(library) {
+  return Boolean(LIBRARIES[library])
+}
+
+export function getLibraryMetadata(library) {
+  return LIBRARIES[library]
+}
+
+export function registerLibraryStylesheet(library, stylesheet) {
+  getLibraryMetadata(library)?.stylesheets.push(stylesheet)
+}
+
+function loadStylesheet(stylesheet) {
+  console.log('[HOST] Dynamically importing', stylesheet)
+  const link = document.createElement('link')
+  link.setAttribute('rel', 'stylesheet')
+  link.setAttribute('type', 'text/css')
+  link.setAttribute('href', stylesheet)
+  document.head.appendChild(link)
+}
+
+export function loadStylesheetsFromLibrary(library) {
+  if (library.stylesheetsLoaded) return
+
+  for (const stylesheet of library.stylesheets) {
+    if (library.isDevelopment) {
+      loadStylesheet(`${library.cdn}/${stylesheet}`)
+    } else {
+      loadStylesheet(`${library.cdn}/${library.npm.root}/${stylesheet}`)
+    }
+    library.stylesheetsLoaded = true
+  }
+}
+
+function parseNpmCoords(coords, prefixLength = 0) {
+  const parts = coords.split('/').filter(x => x).slice(prefixLength)
+  const scope = parts[0][0] === '@' ? parts[0] : ''
+  const nameParts = scope ? parts[1].split('@') : parts[0].split('@')
+  const name = nameParts[0]
+  const version = nameParts[1] || ''
+  const path = (scope ? parts.slice(2) : parts.slice(1)).join('/')
+  const fullname = scope ? scope + '/' + name : name
+  const root = `${fullname}${version ? '@' + version : ''}`
+
+  return { fullname, scope, name, version, path, root }
+}
+
+for (const [library, origin] of Object.entries(LIBRARIES)) {
+  const isAbsoluteOrigin = origin.startsWith('http://') || origin.startsWith('https://')
+  const isDevelopment = origin.startsWith('http://localhost:')
+  const url = new URL(isAbsoluteOrigin ? origin : window.location.origin + origin)
+  const cdn = isDevelopment ? url.origin : isAbsoluteOrigin ? origin : '/package'
+  const prefixLength = isAbsoluteOrigin ? 0 : 1
+  const npm = isDevelopment ? parseNpmCoords(library) : parseNpmCoords(url.pathname, prefixLength)
+
+  LIBRARIES[library] = {
+    name: library,
+    npm,
+    cdn,
+    isDevelopment,
+    stylesheets: [],
+    stylesheetsLoaded: false,
+  }
+}
--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,17 @@
 import { events } from '@padcom/mf-test-common'
-import { createApp, defineAsyncComponent } from 'vue'
+import { createApp } from 'vue'
+
+import {
+  registerLibraryStylesheet,
+  getLibraryMetadata,
+  isLibraryRegistered,
+  loadStylesheetsFromLibrary,
+} from './libraries.js'
+
+registerLibraryStylesheet('@padcom/mf-test-library3', 'dist/style.css')
+registerLibraryStylesheet('@padcom/mf-test-library5', 'dist/style.css')
+registerLibraryStylesheet('@padcom/mf-test-library6', 'dist/style.css')
+registerLibraryStylesheet('@padcom/mf-test-library7', 'dist/style.css')
 
 // Vue.js and React require this (simulates dotenv)
 globalThis.process = { env: { NODE_ENV: 'development' } }
@@ -7,60 +19,40 @@ globalThis.process = { env: { NODE_ENV: 'development' } }
 globalThis.__VUE_OPTIONS_API__ = false
 globalThis.__VUE_PROD_DEVTOOLS__ = false
 
-async function sleep(ms, msg = `[HOST] System initialization in progress - waiting ${ms / 1000} seconds...`) {
-  // if (msg) console.log(msg)
-  // return new Promise(resolve => setTimeout(resolve, ms))
-}
+async function start(root, library) {
+  if (!isLibraryRegistered(library)) throw new Error('Unknown library', library)
+  library = getLibraryMetadata(library)
 
-function loadStylesheets(stylesheets) {
-  for (const stylesheet of stylesheets) {
-    loadStylesheet(stylesheet)
-  }
-}
+  console.time('[HOST] Starting microfrontend ' + library.npm.fullname + ' took')
 
-function loadStylesheet(stylesheet) {
-  console.log('[HOST] Dynamically importing', stylesheet)
-  const link = document.createElement('link')
-  link.setAttribute('rel', 'stylesheet')
-  link.setAttribute('type', 'text/css')
-  link.setAttribute('href', stylesheet)
-  document.head.appendChild(link)
-}
+  loadStylesheetsFromLibrary(library)
 
-async function start(root, url, stylesheets = []) {
-  console.time('[HOST] Starting microfrontend ' + url + ' took')
-  if (!Array.isArray(stylesheets)) stylesheets = [ stylesheets ]
-
-  loadStylesheets(stylesheets)
-
-  console.log(`[HOST] Dynamically importing ${url} exports...`)
-  const { start } = await import(url)
+  console.log(`[HOST] Dynamically importing ${library} exports...`)
+  const { start } = await import(library.name)
   console.log('[HOST] Exports loaded:')
   console.log('[HOST] > start =', start)
   console.log('')
-
-  await sleep(1000)
 
   console.log('[HOST] Instantiating microfrontend')
   const app = await start(root, 'Jane Smith')
   console.log('[HOST] app =', app)
   console.log('')
 
-  await sleep(1000)
-
-  console.timeEnd('[HOST] Starting microfrontend ' + url + ' took')
+  console.timeEnd('[HOST] Starting microfrontend ' + library.npm.fullname + ' took')
 
   return app
 }
 
-async function startApp(root, url, stylesheets = []) {
-  console.time('[HOST] Starting application ' + url + ' took')
-  if (!Array.isArray(stylesheets)) stylesheets = [ stylesheets ]
+async function startApp(root, library) {
+  if (!isLibraryRegistered(library)) throw new Error('Unknown library', library)
+  library = getLibraryMetadata(library)
 
-  loadStylesheets(stylesheets)
+  console.time('[HOST] Starting application ' + library.npm.fullname + ' took')
 
-  console.log(`[HOST] Dynamically importing ${url} exports...`)
-  const { App } = await import(url)
+  loadStylesheetsFromLibrary(library)
+
+  console.log(`[HOST] Dynamically importing ${library} exports...`)
+  const { App } = await import(library.name)
   console.log('[HOST] Exports loaded:')
   console.log('[HOST] > App =', App)
   console.log('')
@@ -70,35 +62,32 @@ async function startApp(root, url, stylesheets = []) {
   console.log('[HOST] app =', app)
   console.log('')
 
-  await sleep(1000)
-
-  console.timeEnd('[HOST] Starting application ' + url + ' took')
+  console.timeEnd('[HOST] Starting application ' + library.npm.fullname + ' took')
 
   return app
 }
 
 async function main() {
   console.time('[HOST] System initialized.')
-  await sleep(5000, '[HOST] System initialization started - waiting 5 seconds...')
 
   await Promise.all([
     start(document.getElementById('app1'), '@padcom/mf-test-library1'),
     start(document.getElementById('app2'), '@padcom/mf-test-library2'),
-    start(document.getElementById('app3'), '@padcom/mf-test-library3', 'https://unpkg.com/@padcom/mf-test-library3@0.0/dist/style.css'),
+    start(document.getElementById('app3'), '@padcom/mf-test-library3'),
     start(document.getElementById('app4'), '@padcom/mf-test-library4'),
   ])
 
   console.time('[HOST] Instantiating a microfrontend with host-provided and NOT cached dependencies took')
-  await start(document.getElementById('app5'), '@padcom/mf-test-library5', 'https://unpkg.com/@padcom/mf-test-library5@0.0/dist/style.css'),
+  await start(document.getElementById('app5'), '@padcom/mf-test-library5'),
   console.timeEnd('[HOST] Instantiating a microfrontend with host-provided and NOT cached dependencies took')
 
   console.time('[HOST] Instantiating a microfrontend with host-provided and cached dependencies took')
-  const app = await start(document.getElementById('app6'), '@padcom/mf-test-library6', 'https://unpkg.com/@padcom/mf-test-library6@0.0/dist/style.css')
+  const app = await start(document.getElementById('app6'), '@padcom/mf-test-library6')
   console.log('[HOST] Removing last app so it can be instantiated using clickme from library5')
   app.unmount()
   console.log('[HOST] App removed')
   // await start(document.getElementById('app6'), 'http://localhost:3006/index.js', 'http://localhost:3006/style.css'),
-  await start(document.getElementById('app6'), '@padcom/mf-test-library6', 'https://unpkg.com/@padcom/mf-test-library6@0.0/dist/style.css')
+  await start(document.getElementById('app6'), '@padcom/mf-test-library6')
   console.timeEnd('[HOST] Instantiating a microfrontend with host-provided and cached dependencies took')
 
   window.addEventListener('message', async (event) => {
