b9d490a55c0b19aa9cf6bee42cc9090ec7f45c71
Matthias Hryniszak <padcom@gmail.com>
2022-07-28 22:25:29 +0200

    Updated docker infrastructure to silence as many warnings as possible

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM node:alpine
 
+ENV NODE_NO_WARNINGS=1
+
+RUN npm config set update-notifier false
+
 RUN mkdir -p /var/lib/npm-serve
 
 WORKDIR /var/lib/npm-serve
 
 EXPOSE 2998
 
-ENV NODE_NO_WARNINGS=1
-
-CMD [ "npx", "--yes", "@padcom/npm-serve", "static-files" ]
+CMD [ "npx", "--silent", "--yes", "@padcom/npm-serve", "static-files" ]
--- docker-run-alpine.sh
+++ docker-run-alpine.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -it -p 2998:2998 -v $(pwd)/docs:/var/lib/npm-serve/static-files node:alpine npx -y @padcom/npm-serve /var/lib/npm-serve/static-files -q
+docker run --rm -it -p 2998:2998 -v $(pwd)/docs:/var/lib/npm-serve/static-files -e NODE_NO_WARNINGS=1 node:alpine npx --silent -y @padcom/npm-serve /var/lib/npm-serve/static-files -q
